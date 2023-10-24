bfb9f17548784ab9527ce72845bbf637fe9da445
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2023-10-25 08:37:03 +0900

    Initial Commit

---
+++ Music_Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music_Player
+//
+//  Created by 岸　優樹 on 2023/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music_Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music_Player
+//
+//  Created by 岸　優樹 on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
