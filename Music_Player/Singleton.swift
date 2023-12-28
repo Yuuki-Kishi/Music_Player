652d76bcbdecbff09c04c9b35db2cb1179bcb9d5
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2023-12-28 22:41:43 +0900

    UI改良、シングルトン追加、画面追加

---
+++ Music_Player/Singleton.swift
@@ -0,0 +1,8 @@
+//
+//  Singleton.swift
+//  Music_Player
+//
+//  Created by 岸　優樹 on 2023/12/28.
+//
+
+import Foundation
---
+++ Music_Player/listMusic.swift
@@ -0,0 +1,18 @@
+//
+//  listMusic.swift
+//  Music_Player
+//
+//  Created by 岸　優樹 on 2023/12/28.
+//
+
+import SwiftUI
+
+struct listMusic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listMusic()
+}
