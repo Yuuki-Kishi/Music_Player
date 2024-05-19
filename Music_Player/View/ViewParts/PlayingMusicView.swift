a54ade254f790ef86d491ff26f0d2a1f5841f87e
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2024-05-19 22:14:14 +0900

    PlayingMusicViewのforce unlapを修正

--- Music_Player/View/ViewParts/PlayingMusicView.swift
+++ Music_Player/View/ViewParts/PlayingMusicView.swift
@@ -35,11 +35,11 @@ struct PlayingMusicView: View {
                             .font(.system(size: 20.0))
                             .frame(maxWidth: .infinity, alignment: .leading)
                         HStack {
-                            Text(music?.artistName! ?? "")
+                            Text(music?.artistName ?? "不明なアーティスト")
                                 .lineLimit(1)
                                 .font(.system(size: 12.5))
                                 .frame(maxWidth: .infinity, alignment: .leading)
-                            Text(music?.albumName! ?? "")
+                            Text(music?.albumName ?? "不明なアルバム")
                                 .lineLimit(1)
                                 .font(.system(size: 12.5))
                                 .frame(maxWidth: .infinity,alignment: .leading)
